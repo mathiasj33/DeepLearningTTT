88fe7fa494245931380bf58822017151c52f03fe
mathiasj33 <mathias.jackermeier@tum.de>
2017-06-14 20:43:20 +0200

    Implemented a working version of the feedforward code

--- main/main.py
+++ main/main.py
@@ -6,43 +6,34 @@ from game import Game
 import visualize
 from neuralnet import net
 from PIL import Image
-import neuralnet.example as example
+import scipy
+import numpy as np
 
 os.environ['GLOG_minloglevel'] = '0' 
 
+def classify_example():
+    nn = net.NeuralNet('20170607-223813-dc64', 4800, '20170607-223705-f269')
+    example = Image.open('/home/mathias/example.png').convert('L').resize((28,28),Image.BILINEAR)
+    print nn.get_result(example) + 1
+
 def main():
     game = Game('X')
+    pos_map_inv = {(0, 0): 1, (1, 0): 2, (2, 0): 3, (0, 1): 4, (1, 1): 5, (2, 1): 6, (0, 2): 7, (1, 2): 8, (2, 2): 9}
+    pos_map = {v: k for k, v in pos_map_inv.iteritems()}
     
-    nn = net.NeuralNet()
-    image = Image.open('/home/mathias/mnist/train/2/00178.png')
-    result = nn.get_result(image)
-    print result
+    nn = net.NeuralNet('20170607-223813-dc64', 4800, '20170607-223705-f269')
     
-    
-    #===========================================================================
-    # result = example.classify(
-    #         '/home/mathias/Cloned_repos/DIGITS/digits/jobs/20170610-012554-8beb/snapshot_iter_1408.caffemodel',
-    #         '/home/mathias/Cloned_repos/DIGITS/digits/jobs/20170610-012554-8beb/deploy.prototxt',
-    #         ['/home/mathias/mnist/train/2/00178.png'],
-    #         mean_file='/home/mathias/Cloned_repos/DIGITS/digits/jobs/20170401-220250-9164/mean.binaryproto',
-    #         use_gpu=False
-    #     )
-    # print result.argmax(1)
-    # print result[0,result.argmax(1)]
-    #===========================================================================
-    
-    #===========================================================================
-    # while True:
-    #     i = raw_input()
-    #     game.set_figure((int(i[0]), int(i[1])))
-    #     image = visualize.create_image(game)
-    #     #visualize.draw(game)
-    #     image = image.resize((28,28), PIL.Image.ANTIALIAS)
-    #     result = nn.get_result(image)
-    #     print result
-    #     game.set_figure(result)
-    #     #visualize.draw(game)
-    #===========================================================================
+    while True:
+        i = raw_input()
+        game.set_figure((int(i[0]), int(i[1])))
+        image = visualize.create_image(game)
+        visualize.draw(game)
+        image = image.resize((28,28), Image.BILINEAR)
+        result = nn.get_result(image) + 1
+        print result
+        pos = pos_map[result]
+        game.set_figure(pos)
+        visualize.draw(game)
         
 if __name__ == '__main__':
     main()
--- neuralnet/net.py
+++ neuralnet/net.py
@@ -1,23 +1,18 @@
-import caffe
 import numpy as np
-from PIL import Image
 from caffe.proto import caffe_pb2
 import caffe.io
 
 class NeuralNet:
     
-    def __init__(self):
+    def __init__(self, job_id, iteration, dataset_job_id):
         caffe.set_mode_cpu()
-        path = '/home/mathias/Cloned_repos/DIGITS/digits/jobs/20170401-220343-caab/'
-        self.net = caffe.Net(path + 'deploy.prototxt', path + 'snapshot_iter_1408.caffemodel', caffe.TEST)
+        path = '/home/mathias/Cloned_repos/DIGITS/digits/jobs/{}/'.format(job_id)
+        self.net = caffe.Net(path + 'deploy.prototxt', path + 'snapshot_iter_{}.caffemodel'.format(iteration), caffe.TEST)
         
-        self.load_mean()
-        
-        pos_map_inv = {(0, 0): 1, (1, 0): 2, (2, 0): 3, (0, 1): 4, (1, 1): 5, (2, 1): 6, (0, 2): 7, (1, 2): 8, (2, 2): 9}
-        self.pos_map = {v: k for k, v in pos_map_inv.iteritems()}
+        self.load_mean(dataset_job_id)
     
-    def load_mean(self):
-        with open('/home/mathias/Cloned_repos/DIGITS/digits/jobs/20170401-220250-9164/mean.binaryproto', 'rb') as infile:
+    def load_mean(self, dataset_job_id):
+        with open('/home/mathias/Cloned_repos/DIGITS/digits/jobs/{}/mean.binaryproto'.format(dataset_job_id), 'rb') as infile:
             blob = caffe_pb2.BlobProto()
             blob.MergeFromString(infile.read())
             mean_arr = caffe.io.blobproto_to_array(blob)
@@ -37,5 +32,5 @@ class NeuralNet:
         self.net.blobs['data'].data[...] = image
         output = self.net.forward()
         probs = output['softmax']
-        print 'Probs:' + str(probs)
+        print probs
         return probs
